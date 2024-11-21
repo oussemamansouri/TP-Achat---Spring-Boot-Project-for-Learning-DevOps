00dc2b4523072c282c5de66088a0ffe8d1e6cfac
oussemamansouri <oussemamansouri619@gmail.com>
2024-11-21 19:23:39 +0100

    Update ProduitRestController.java

--- src/main/java/com/esprit/examen/controllers/ProduitRestController.java
+++ src/main/java/com/esprit/examen/controllers/ProduitRestController.java
@@ -1,10 +1,18 @@
 package com.esprit.examen.controllers;
 
-import java.util.Date;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.esprit.examen.entities.Produit;
 import com.esprit.examen.services.IProduitService;
 
@@ -17,8 +25,13 @@ import io.swagger.annotations.Api;
 @RequestMapping("/produit")
 public class ProduitRestController {
 
-	@Autowired
-	IProduitService produitService;
+
+	
+	final IProduitService produitService;
+	public ProduitRestController(IProduitService produitService){
+		this.produitService = produitService;
+	}
+	
 
 	// http://localhost:8089/SpringMVC/produit/retrieve-all-produits
 	@GetMapping("/retrieve-all-produits")
