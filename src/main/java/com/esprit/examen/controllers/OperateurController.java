0f24cddddfd4b56dd6041f50c563904ac7890845
oussemamansouri <oussemamansouri619@gmail.com>
2024-11-21 19:23:37 +0100

    Update OperateurController.java

--- src/main/java/com/esprit/examen/controllers/OperateurController.java
+++ src/main/java/com/esprit/examen/controllers/OperateurController.java
@@ -2,8 +2,16 @@ package com.esprit.examen.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.esprit.examen.entities.Operateur;
 import com.esprit.examen.services.IOperateurService;
 
@@ -15,12 +23,14 @@ import io.swagger.annotations.Api;
 @CrossOrigin("*")
 public class OperateurController {
 
-	@Autowired
-	IOperateurService operateurService;
+
+		final IOperateurService operateurService;
+	public OperateurController(IOperateurService operateurService){
+		this.operateurService = operateurService;
+	}
 	
 	// http://localhost:8089/SpringMVC/operateur/retrieve-all-operateurs
 	@GetMapping("/retrieve-all-operateurs")
-	@ResponseBody
 	public List<Operateur> getOperateurs() {
 		List<Operateur> list = operateurService.retrieveAllOperateurs();
 		return list;
@@ -28,14 +38,12 @@ public class OperateurController {
 
 	// http://localhost:8089/SpringMVC/operateur/retrieve-operateur/8
 	@GetMapping("/retrieve-operateur/{operateur-id}")
-	@ResponseBody
 	public Operateur retrieveOperateur(@PathVariable("operateur-id") Long operateurId) {
 		return operateurService.retrieveOperateur(operateurId);
 	}
 
 	// http://localhost:8089/SpringMVC/operateur/add-operateur
 	@PostMapping("/add-operateur")
-	@ResponseBody
 	public Operateur addOperateur(@RequestBody Operateur op) {
 		Operateur operateur = operateurService.addOperateur(op);
 		return operateur;
@@ -43,14 +51,12 @@ public class OperateurController {
 
 	// http://localhost:8089/SpringMVC/operateur/remove-operateur/{operateur-id}
 	@DeleteMapping("/remove-operateur/{operateur-id}")
-	@ResponseBody
 	public void removeOperateur(@PathVariable("operateur-id") Long operateurId) {
 		operateurService.deleteOperateur(operateurId);
 	}
 
 	// http://localhost:8089/SpringMVC/operateur/modify-operateur
 	@PutMapping("/modify-operateur")
-	@ResponseBody
 	public Operateur modifyOperateur(@RequestBody Operateur operateur) {
 		return operateurService.updateOperateur(operateur);
 	}
