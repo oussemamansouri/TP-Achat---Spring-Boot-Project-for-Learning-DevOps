2db99718cc229f0158d6145096adb26a94745aa3
oussemamansouri <oussemamansouri619@gmail.com>
2024-11-21 19:23:41 +0100

    Update ReglementRestController.java

--- src/main/java/com/esprit/examen/controllers/ReglementRestController.java
+++ src/main/java/com/esprit/examen/controllers/ReglementRestController.java
@@ -3,10 +3,14 @@ package com.esprit.examen.controllers;
 import java.util.Date;
 import java.util.List;
 
-import com.esprit.examen.entities.Facture;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.esprit.examen.entities.Reglement;
 import com.esprit.examen.services.IReglementService;
@@ -19,19 +23,20 @@ import io.swagger.annotations.Api;
 @CrossOrigin("*")
 public class ReglementRestController {
 
-    @Autowired
-    IReglementService reglementService;
+
+    final IReglementService reglementService;
+	public ReglementRestController(IReglementService reglementService){
+		this.reglementService = reglementService;
+	}
 
 
     // http://localhost:8089/SpringMVC/reglement/add-reglement
     @PostMapping("/add-reglement")
-    @ResponseBody
     public Reglement addReglement(@RequestBody Reglement r) {
         Reglement reglement = reglementService.addReglement(r);
         return reglement;
     }
     @GetMapping("/retrieve-all-reglements")
-    @ResponseBody
     public List<Reglement> getReglement() {
         List<Reglement> list = reglementService.retrieveAllReglements();
         return list;
@@ -39,14 +44,12 @@ public class ReglementRestController {
 
     // http://localhost:8089/SpringMVC/reglement/retrieve-reglement/8
     @GetMapping("/retrieve-reglement/{reglement-id}")
-    @ResponseBody
     public Reglement retrieveReglement(@PathVariable("reglement-id") Long reglementId) {
         return reglementService.retrieveReglement(reglementId);
     }
 
     // http://localhost:8089/SpringMVC/reglement/retrieveReglementByFacture/8
     @GetMapping("/retrieveReglementByFacture/{facture-id}")
-    @ResponseBody
     public List<Reglement> retrieveReglementByFacture(@PathVariable("facture-id") Long factureId) {
         return reglementService.retrieveReglementByFacture(factureId);
     }
